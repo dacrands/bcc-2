e8e70b190981f4ae7fce28ab9ed508e28a84edc6
da crands <davecrands@gmail.com>
2018-03-11 12:27:10 -0400

    Particles landing

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+const about = document.querySelector('.about');
+const landing = document.querySelector('.landing');
+const nav = document.querySelector('.navbar');
+const navIcon = document.querySelector('navbar-icon')
+
+window.addEventListener('scroll', () =>{
+    if (window.scrollY >= about.scrollHeight - 180) {
+        nav.classList.remove('navbar-landing');
+        nav.classList.add('navbar-bg');
+        navIcon.classList.remove('fa-2x');
+    } else {
+        nav.classList.add('navbar-landing');    
+        nav.classList.remove('navbar-bg');
+        navIcon.classList.add('fa-2x');
+    }
+});
